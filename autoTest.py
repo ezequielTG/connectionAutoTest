a138de6a79c968d2dd2982aa76d50ceb643454be
ezequiel t gomes <70965840+ezequielTG@users.noreply.github.com>
2023-11-23 13:49:47 -0300

    Documentando autoTest.py
    Foram adicionados comentários para explicar a finalidade de algumas variáveis.

--- autoTest.py
+++ autoTest.py
@@ -3,10 +3,19 @@ from selenium.webdriver.common.by import By
 from runTest import run_test
 import pandas as pd
 
+
+#Endereço do site e as informações para que o servidor identifique 
+#quem está enviando a requisição 
+
 url = 'https://www.speedtest.net/pt'
 headers = {
     'User-Agent': 'Mozilla/5.0 (<system-information>)\
      <platform> (<platform-details>) <extensions>'}
+
+#repeat -> Número de vezes que o teste será realizado
+#runt_ime -> O tempo estimado para a execução do teste
+#load_repeat -> Intervalo para o próximo teste
+#load_pg -> Tempo estimado para carregamento de página completo
 
 repeat = 2
 load_repeat = 30
@@ -20,6 +29,8 @@ dic_test_result = {'download': [], 'upload': []}
 driver = webdriver.Chrome()
 
 run_test(driver, repeat, ls_time, dic_test_result, url, By)
+
+#gera a tabela, converte para .csv e salva em downloads
 
 df = pd.DataFrame(dic_test_result)
 df.to_csv('~/Downloads/teste_de_conexão.csv', encoding='utf-8', sep=';')
